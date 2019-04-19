8ba1b76d05d479d3c5927adcd55e628e996c6078
TAY TS <taytzushieh@gmail.com>
2019-04-19 16:11:52 +0800

    feature: Update user db models

--- models/users.py
+++ models/users.py
@@ -1,5 +1,5 @@
 from models.db import db
-from sqlalchemy.dialects.mysql import TIMESTAMP
+from sqlalchemy.dialects.mysql import TIMESTAMP, TINYINT
 from werkzeug.security import check_password_hash
 from datetime import datetime
 
@@ -9,10 +9,10 @@ class Users(db.Model):
     id_user = db.Column(db.Integer, primary_key=True)
     id_user_hash = db.Column(db.String(255), unique=True, default='')
     id_chat = db.Column(db.String(255), unique=True, default='')
+    is_admin = db.Column(TINYINT(3), default=0)
     username = db.Column(db.String(255), default='')
     password = db.Column(db.String(255), default='')
     email = db.Column(db.String(255), unique=True, default='')
-    profile_img_url = db.Column(db.String(500), default='')
     create_timestamp = db.Column(
         TIMESTAMP, default=datetime.utcnow().replace(microsecond=0))
     # TODO: User tier and classification
